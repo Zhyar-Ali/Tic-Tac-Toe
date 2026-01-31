15e82c69d623aa2c51a8c77e600ce223d4f7d7a8
Zhyar <zhiarali595@gmail.com>
2026-01-31 17:59:44 +0300

    make board function with no win check

--- script.js
+++ script.js
@@ -129,8 +129,6 @@ const gameFlow = (function (){
 
 })();
 
-// const player = gameFlow.player("zhyar","x");
-// const player2 = gameFlow.player("wa","o");
 // const win = gameFlow.winner(player,player2);
 
 // gameFlow.gameboardUpdate([0,0],player.mark);
@@ -151,44 +149,68 @@ const dom = (function() {
 
     const board = gameFlow.gameBoard();
     const grid = document.querySelector(".gridLayout");
+
     const submitButtonOne = document.getElementById("submitOne");
     const submitButtonTwo = document.getElementById("submitTwo");
+
+    let p1 = gameFlow.player("","x");
+    let p2 = gameFlow.player("","o");
+
+    function submitClickOne (event){
+        event.preventDefault();
+        const name = document.getElementById("playerOneName").value;
+        const mark = document.getElementById("playerOneMark").value;
+        p1 = gameFlow.player(name,mark);
+    }
+
+    function submitClickTwo (event){
+        event.preventDefault();
+        const name = document.getElementById("playerTwoName").value;
+        const mark= document.getElementById("playerTwoMark").value;
+        p2 = gameFlow.player(name,mark);
+    }
 
     const createDiv = () => {
         for (let i=0; i<board.length; i++){
             for (let j=0; j<board[i].length; j++){
                 const div = document.createElement("div");
                 div.classList.add("box");
+                div.id = `${i},${j}`;
                 div.textContent = board[i][j];
-                grid.appendChild(div);
+                grid.appendChild(div);         
             }
         }
     };
 
-    function submitClickOne (event){
-        event.preventDefault();
-        const name = document.getElementById("playerOneName").value;
-        const mark = document.getElementById("playerOneMark").value;
-        console.log(name + mark);
-    }
-
-     function submitClickTwo (event){
-        event.preventDefault();
-        const name = document.getElementById("playerTwoName").value;
-        const mark= document.getElementById("playerTwoMark").value;
-        console.log(name + mark);
-    }
-
-    const test = () => {
+    const clickEvent = () => {
         submitButtonOne.addEventListener("click", submitClickOne);
         submitButtonTwo.addEventListener("click", submitClickTwo);
     };
 
-    return { createDiv, test};
+
+    let turn = 1;
+    const updateBoard = () => {
+        for (let i=0; i<grid.children.length;i++){
+            let arr = grid.children[i].id.split(",");
+            grid.children[i].addEventListener("click", () => {
+                if (turn === 1){
+                    gameFlow.gameboardUpdate(arr,p1.mark);
+                    grid.children[i].textContent = board[arr[0]][arr[1]];
+                    turn = 2;
+                }else if (turn === 2){
+                    gameFlow.gameboardUpdate(arr,p2.mark);
+                    grid.children[i].textContent = board[arr[0]][arr[1]];
+                    turn = 1;
+                }
+                
+            });
+        }
+    };
+
+    return { createDiv, clickEvent,updateBoard};
 
 })();
 
 dom.createDiv();
-dom.test();
-
-
+dom.clickEvent();
+dom.updateBoard();
